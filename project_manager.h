dbe401728a57c846b3aed3b87463a09274d443c0
Jörg Habenicht <jh@mwerk.net>
2016-03-04 16:49:35 +0100

    add project manager module

---
+++ project_manager.h
@@ -0,0 +1,37 @@
+/*
+ * project_manager.h
+ *
+ *  Created on: 04.03.2016
+ *      Author: jh
+ */
+
+/*
+    Management module for the projects.
+    Organize startup and shutdown of a whole project.
+
+    Copyright (C) 2015,2016  Jörg Habenicht (jh@mwerk.net)
+
+    This file is part of qgis-server-scheduler
+
+    qgis-server-scheduler is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    qgis-server-scheduler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef PROJECT_MANAGER_H_
+#define PROJECT_MANAGER_H_
+
+
+
+#endif /* PROJECT_MANAGER_H_ */
