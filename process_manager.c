789ed20d3ab73b825bc0d4636a0c38ffddc38d20
Jörg Habenicht <jh@mwerk.net>
2016-03-08 17:14:12 +0100

    function to react on dying processes rewritten: call only database functions

--- process_manager.c
+++ process_manager.c
@@ -33,7 +33,12 @@
 #include "process_manager.h"
 
 #include "database.h"
+#include "qgis_config.h"
+#include "qgis_shutdown_queue.h"
+#include "project_manager.h"
 
+
+static const int max_nr_process_crashes = 5;
 
 
 /* a child process died.
@@ -47,7 +52,47 @@
  */
 void process_manager_process_died(pid_t pid)
 {
-    db_remove_process(pid);
+    int retval = db_has_process(pid);
+    if (retval)
+    {
+	/* check if we are during shutdown sequence. if not then restart the
+	 * process.
+	 * check if the process died during the initialisation sequence
+	 * if it did, remark the process being instable.
+	 * then start a new one.
+	 * Refrain from restarting if too much processes have died during the
+	 * initialization.
+	 */
+	retval = get_program_shutdown();
+	if (!retval)
+	{
+	    const char *projname = db_get_project_for_this_process(pid);
+	    enum db_process_list_e proclist = db_get_process_list(pid);
+	    if (LIST_INIT == proclist)
+	    {
+		/* died during initialization. if this happens every time,
+		 * we do get a startup loop. stop after some (5) tries.
+		 */
+		db_inc_startup_failures(projname);
+	    }
+	    retval = db_get_startup_failures(projname);
+	    if (max_nr_process_crashes > retval)
+	    {
+		project_manager_start_new_process_detached(1, projname, 0);
+	    }
+	}
+
+	/* change state of the process to STATE_EXIT
+	 * and move the entry to the shutdown list
+	 */
+	db_process_set_state_exit(pid);
+	qgis_shutdown_add_process(pid);
+    }
+    else
+    {
+	printlog("got signal SIGCHLD but pid %d does not belong to us", pid);
+    }
+//    db_remove_process(pid);
 }
 
 
