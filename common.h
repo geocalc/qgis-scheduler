af0d660c9cfe037d2fd6392fb7dbd174860bf56c
Jörg Habenicht <jh@mwerk.net>
2016-04-02 12:56:25 +0200

    added header to store common definitions

---
+++ common.h
@@ -0,0 +1,36 @@
+/*
+ * common.h
+ *
+ *  Created on: 02.04.2016
+ *      Author: jh
+ */
+
+/*
+    Header for common and project wide definitions.
+
+    Copyright (C) 2015,2016  Jörg Habenicht (jh@mwerk.net)
+
+    This file is part of qgis-server-scheduler
+
+    qgis-server-scheduler is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    qgis-server-scheduler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+
+
+#endif /* COMMON_H_ */
