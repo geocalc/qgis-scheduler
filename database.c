c1284e4659821537da84b690dbd520c44425185f
Jörg Habenicht <jh@mwerk.net>
2016-05-26 16:50:45 +0200

    in database module renamed two inotify functions

--- database.c
+++ database.c
@@ -1896,7 +1896,7 @@ void db_reset_startup_failures(const char *projname)
 }
 
 
-int db_add_new_inotify_watchd(const char *projectname, const char *path, int watchd)
+int db_add_new_inotify_path(const char *projectname, const char *path, int watchd)
 {
     assert(path);
 
@@ -2030,7 +2030,7 @@ int db_get_num_watchd_from_config(const char *path)
 }
 
 
-void db_remove_inotify_configpath(const char *projectname, const char *path)
+void db_remove_inotify_path(const char *projectname, const char *path)
 {
     assert(path);
 
