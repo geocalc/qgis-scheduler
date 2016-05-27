917ba5b8714383aabbdbd8fbbcfda7081f3987a0
Jörg Habenicht <jh@mwerk.net>
2016-05-27 09:53:22 +0200

    added module for string handling functions

---
+++ stringext.h
@@ -0,0 +1,36 @@
+/*
+ * stringext.h
+ *
+ *  Created on: 27.05.2016
+ *      Author: jh
+ */
+
+/*
+    Extended string handling.
+    Provides extended functionality in string functions.
+
+    Copyright (C) 2015,2016  Jörg Habenicht (jh@mwerk.net)
+
+    This file is part of qgis-server-scheduler
+
+    qgis-server-scheduler is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    qgis-server-scheduler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef STRINGEXT_H_
+#define STRINGEXT_H_
+
+
+
+#endif /* STRINGEXT_H_ */
